1980dbd8236f4e460dd9d45f1c3c49577c3688e9
Bach Davis (School) <bdavis@student.newmarkeducation.com>
2025-10-08 18:18:16 +0000

    fixed physics engine user error

--- lib/main.py
+++ lib/main.py
@@ -19,6 +19,7 @@ draw_options = pygame_util.DrawOptions(screen)
 
 player1 = player(100, 100, ("blue_player.png", "blue_player_hold.png"))
 space.add(player1.body, player1.shape)
+# player1.body.apply_impulse_at_local_point((600, -1000), (0, 0))
 
 while running:
     space.step(1 / 60)
